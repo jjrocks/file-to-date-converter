651579a6fcca16920d4027b2445e6a16d745dc8d
JJ Wanda <jjwanda@gmail.com>
2014-04-07 18:51:58 -0400

    So I decided to never touch negation again. Though the mp3 file doesn't work

--- listDirectory.py
+++ listDirectory.py
@@ -3,7 +3,7 @@ from os import listdir
 from os.path import isfile, join
 mypath = "."
 #onlyfiles = [ f.strip("[JJ Log |.aup") for f in listdir(mypath) if isfile(join(mypath,f)) ]
-onlyfiles = [ re.sub(r'\d+-\d+-\d+', '', f) for f in listdir(mypath) if isfile(join(mypath,f)) ]
+onlyfiles = [ re.sub(r'[\.(mp3)|a-z|A-Z|\.|\s]', '', f) for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
 
 print(onlyfiles)
--- regExpres.py
+++ regExpres.py
@@ -1,7 +1,8 @@
 import re
 
-dateList = ["JJ Log 5-7-11", "JJ Log 5-8-11", "JJ Log 5-9-11", "JJ Log 5-10-11", "JJ Log 4-7-11", "JJ Log 12-13-11", "JJ Log 11-15-11", "JJ Log 5-11-11"]
+dateList = ["JJ Log 5-7-11.aup", "JJ Log 5-8-11.aup", "JJ Log 5-9-11.aup", "JJ Log 5-10-11.aup", "JJ Log 4-7-11.aup", "JJ Log 12-13-11.aup", "JJ Log 11-15-11.aup", "JJ Log 5-11-11.aup"]
+
 
 for dates in dateList:
-	dates = re.sub(r'(?!o)', '', dates)
+	dates = re.sub(r'[a-z|A-Z|\.|\s]', '', dates)
 	print(dates)
