42416b1cc05eaa0aaa51bdcf4a63a75194b90246
JJ Wanda <jjwanda@gmail.com>
2014-04-07 15:37:05 -0400

    Added a reg expression tester since my other computer does not have the files

--- listDirectory.py
+++ listDirectory.py
@@ -3,10 +3,7 @@ from os import listdir
 from os.path import isfile, join
 mypath = "."
 #onlyfiles = [ f.strip("[JJ Log |.aup") for f in listdir(mypath) if isfile(join(mypath,f)) ]
-onlyfiles = [ re.match(r'\d+-\d+-\d+', f) for f in listdir(mypath) if isfile(join(mypath,f)) ]
+onlyfiles = [ re.sub(r'\d+-\d+-\d+', '', f) for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
-for files in onlyfiles:
-	if(files.group()):
-		print(files.group())
 
 print(onlyfiles)
---
+++ regExpres.py
@@ -0,0 +1,9 @@
+import re
+
+dateList = ["JJ Log 5-7-11", "JJ Log 5-8-11", "JJ Log 5-9-11", "JJ Log 5-10-11", "JJ Log 4-7-11", "JJ Log 12-13-11", "JJ Log 11-15-11", "JJ Log 5-11-11"]
+
+for dates in dateList:
+	dates = re.sub(r'\d+-\d+-\d+.*', '', dates)
+	print(dates)
+
+print(dateList)
