a00d0c457d2a50817126c8a55da5b4023aaaf7c1
jjrocks <jjfunman@gmail.com>
2014-04-07 20:56:32 -0400

    Well I got something that worked. Though having digits in there did not help

--- regExpres.py
+++ regExpres.py
@@ -1,8 +1,12 @@
 import re
 
-dateList = ["JJ Log 5-7-11.aup", "JJ Log 5-8-11.aup", "JJ Log 5-9-11.aup", "JJ Log 5-10-11.aup", "JJ Log 4-7-11.aup", "JJ Log 12-13-11.aup", "JJ Log 11-15-11.aup", "JJ Log 5-11-11.aup"]
+dateList = ["JJ Log 5-7-11.aup", "JJ Log 5-8-11.mp3", "JJ Log 5-9-11.mp3", "JJ Log 5-10-11.aup", "JJ Log 4-7-11.aup", "JJ Log 12-13-11.aup", "JJ Log 11-15-11.mp3", "JJ Log 3-13-33.mp3"]
 
 
 for dates in dateList:
-	dates = re.sub(r'[a-z|A-Z|\.|\s]', '', dates)
+	#dates = re.sub(r'[(\|a-z|A-Z|\.|\s]', '', dates)
+	print("----------------")
+	dates = dates.strip("mp3")
+	print(dates)
+	dates = re.sub(r'[^\d|\-]', '', dates)
 	print(dates)
