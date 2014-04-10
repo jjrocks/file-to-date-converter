aa90f25b9233dd412f32b5eeb962761cf86eb91a
JJ Wanda <jjwanda@gmail.com>
2014-04-10 17:15:08 -0400

    So I actually am nearly done with checking to see if you have to listen to one, just gotta add a messsage

--- listDirectory.py
+++ listDirectory.py
@@ -8,8 +8,14 @@ def stringListToDateList(stringList):
 	for dates in stringList:
 		currentDateString = dates.split("-")
 		if len(currentDateString) == 3:
-			dateList.append(int(currentDateString[2]), int(currentDateString[0]), int(currentDateString[1]))
+			dateList.append(date(int(currentDateString[2])+2000, int(currentDateString[0]), int(currentDateString[1])))
 	return dateList
+
+def checkLog(dateList):
+	todayDate = date.today()
+	for dates in dateList:
+		if(date.today() == dates.replace(year = dates.year + 3)):
+			print(dates)
 
 
 
@@ -17,6 +23,5 @@ mypath = "."
 #onlyfiles = [ f.strip("[JJ Log |.aup") for f in listdir(mypath) if isfile(join(mypath,f)) ]
 onlyfiles = [ re.sub(r'[^\d|\-]', '', re.sub(r'\..+', '', f)) for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
-
-
-print(onlyfiles)
+dateList = stringListToDateList(onlyfiles)
+checkLog(dateList)
